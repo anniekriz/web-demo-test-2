3d011ac0516c8c38c8b2cc1eff72c38c9b38c091
ANNIE\annau <anna.ukrizu@gmail.com>
2026-02-18 10:42:23 +0100

    Running web without errors

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -29,9 +29,9 @@ export function AboutSection({ heading, body, image, editable, onChange, pending
         <h2 className={styles.heading}>
           <EditableText value={heading} onChange={(v) => onChange('aboutHeading', v)} editable={editable} className={styles.heading} />
         </h2>
-        <p className={styles.body}>
+        <div className={styles.body}>
           <EditableText value={body} onChange={(v) => onChange('aboutBody', v)} editable={editable} className={styles.body} multiline />
-        </p>
+        </div>
       </div>
     </section>
   )
