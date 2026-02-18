ce786cef16121d45b36a5c21f729ca9e38fcbeb5
ANNIE\annau <anna.ukrizu@gmail.com>
2026-02-18 10:30:51 +0100

    First time running web

--- app/(payload)/api/[...slug]/route.ts
+++ app/(payload)/api/[...slug]/route.ts
@@ -1,8 +1,18 @@
-import { REST_GET, REST_OPTIONS, REST_PATCH, REST_POST, REST_DELETE, REST_PUT } from '@payloadcms/next/routes'
+import config from '../../../../payload.config'
+import '@payloadcms/next/css'
 
-export const GET = REST_GET
-export const POST = REST_POST
-export const DELETE = REST_DELETE
-export const PATCH = REST_PATCH
-export const PUT = REST_PUT
-export const OPTIONS = REST_OPTIONS
+import {
+  REST_DELETE,
+  REST_GET,
+  REST_OPTIONS,
+  REST_PATCH,
+  REST_POST,
+  REST_PUT,
+} from '@payloadcms/next/routes'
+
+export const GET = REST_GET(config)
+export const POST = REST_POST(config)
+export const DELETE = REST_DELETE(config)
+export const PATCH = REST_PATCH(config)
+export const PUT = REST_PUT(config)
+export const OPTIONS = REST_OPTIONS(config)
